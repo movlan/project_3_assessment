a0baa0a62345a1858d7e431a1cab2f692123b0cb
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-27 13:08:06 -0600

    done

--- wish_list/models.py
+++ wish_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Wish(models.Model):
+    text = models.TextField(max_length=200)
+    
+    def get_absolute_url(self):
+        return reverse('index')
--- wish_list/views.py
+++ wish_list/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Wish
 
-# Create your views here.
+class WishCreate(CreateView):
+    model = Wish
+    fields = ['text']
+
+class WishDelete(DeleteView):
+    model = Wish
+    success_url = '/'
+
+def index(request):
+    item_list = Wish.objects.all()
+    return render(request, 'index.html', { 'item_list': item_list,})
